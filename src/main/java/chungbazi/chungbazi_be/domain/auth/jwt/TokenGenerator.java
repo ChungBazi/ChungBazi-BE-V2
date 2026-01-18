4789a2acc34246aec652558a65186efdebe481a5
zyovn <yui2507@naver.com>
2026-01-18 21:01:41 +0900

    [feat] at, rt 만료 시간 환경 변수로 제어 (#173)

--- src/main/java/chungbazi/chungbazi_be/domain/auth/jwt/TokenGenerator.java
+++ src/main/java/chungbazi/chungbazi_be/domain/auth/jwt/TokenGenerator.java
@@ -2,6 +2,7 @@ package chungbazi.chungbazi_be.domain.auth.jwt;
 
 import chungbazi.chungbazi_be.domain.auth.dto.TokenDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -10,16 +11,20 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class TokenGenerator {
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 48; // 2일
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+
+    @Value("${jwt.access-exp}")
+    private long accessTokenExp;
+
+    @Value("${jwt.refresh-exp}")
+    private long refreshTokenExp;
 
     private final JwtProvider jwtProvider;
 
     public TokenDTO generate(Long userId, String userName, Boolean isFirst) {
+        long now = System.currentTimeMillis();
 
-        long now = (new Date()).getTime();
-        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
-        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+        Date accessTokenExpiredAt = new Date(now + accessTokenExp);
+        Date refreshTokenExpiredAt = new Date(now + refreshTokenExp);
 
         String subject = userId.toString();
         String accessToken = jwtProvider.accessTokenGenerate(subject, accessTokenExpiredAt);
@@ -29,10 +34,11 @@ public class TokenGenerator {
                 accessToken,
                 refreshToken,
                 BEARER_TYPE,
-                ACCESS_TOKEN_EXPIRE_TIME / 1000L,
-                REFRESH_TOKEN_EXPIRE_TIME / 1000L,
+                accessTokenExp / 1000L,
+                refreshTokenExp / 1000L,
                 userId,
                 userName,
-                isFirst);
+                isFirst
+        );
     }
 }
