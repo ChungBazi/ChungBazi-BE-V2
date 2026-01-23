adb2dcf3cca1f3c4905817a8c3f0e0972d4656a1
dldusgh318 <pololydotoly@gmail.com>
2026-01-23 23:03:26 +0900

    refactor: 코드리뷰 반영 (#183)

--- src/main/java/chungbazi/chungbazi_be/domain/user/repository/UserRepository.java
+++ src/main/java/chungbazi/chungbazi_be/domain/user/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package chungbazi.chungbazi_be.domain.user.repository;
 
 import chungbazi.chungbazi_be.domain.user.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
