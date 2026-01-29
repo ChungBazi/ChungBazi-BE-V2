b5f034b7b419bb0f807d0c403abaa985578299af
zyovn <yui2507@naver.com>
2026-01-29 15:09:57 +0900

    feat: 로그아웃된 토큰 재사용 방지 로직 추가 (#185)

--- src/main/java/chungbazi/chungbazi_be/domain/auth/service/AuthService.java
+++ src/main/java/chungbazi/chungbazi_be/domain/auth/service/AuthService.java
@@ -242,7 +242,6 @@ public class AuthService {
         return authConverter.toRefreshTokenResponse(token);
     }
 
-
     public boolean determineIsFirst(User user) {
         return !user.isSurveyStatus();
     }
