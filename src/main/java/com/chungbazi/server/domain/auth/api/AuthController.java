ceae468e18fdad47ac600319aec724e5f8d57b94
zyovn <yui2507@naver.com>
2026-06-18 19:39:11 +0900

    docs: swagger 작성 (#3)

--- src/main/java/com/chungbazi/server/domain/auth/api/AuthController.java
+++ src/main/java/com/chungbazi/server/domain/auth/api/AuthController.java
@@ -1,5 +1,6 @@
 package com.chungbazi.server.domain.auth.api;
 
+import com.chungbazi.server.domain.auth.api.docs.AuthDocs;
 import com.chungbazi.server.domain.auth.api.dto.request.KakaoLoginRequest;
 import com.chungbazi.server.domain.auth.api.dto.response.AuthTokenResponse;
 import com.chungbazi.server.domain.auth.application.AuthService;
@@ -13,10 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
-public class AuthController {
+public class AuthController implements AuthDocs {
 
     private final AuthService authService;
 
+    @Override
     @PostMapping("/kakao")
     public CommonResponse<AuthTokenResponse> loginWithKakao(@RequestBody KakaoLoginRequest request) {
         return CommonResponse.onSuccess(authService.loginWithKakao(request));
---
+++ src/main/java/com/chungbazi/server/domain/auth/api/docs/AuthDocs.java
@@ -0,0 +1,32 @@
+package com.chungbazi.server.domain.auth.api.docs;
+
+import com.chungbazi.server.domain.auth.api.dto.request.KakaoLoginRequest;
+import com.chungbazi.server.domain.auth.api.dto.response.AuthTokenResponse;
+import com.chungbazi.server.global.common.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "[Auth]", description = "인증/인가 관련 API")
+public interface AuthDocs {
+    @Operation(
+            summary = "카카오 로그인 API",
+            description = """
+                    ### RequestBody
+                    ---
+                    - `accessToken`: iOS Kakao SDK 로그인 성공 후 발급받은 카카오 accessToken
+                    - `fcmToken`: 현재 로그인한 기기의 fcmToken
+                    """
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "카카오 로그인이 성공적으로 실행됐습니다."
+            )
+    })
+    CommonResponse<AuthTokenResponse> loginWithKakao(
+            @RequestBody KakaoLoginRequest request
+    );
+}
